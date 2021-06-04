5125ec85dfddd5651591fef8b733c9caeccd10f1
Matt Domm <mattdomm@gmail.com>
2021-06-04 13:02:15 +0100

    lowercase

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './src/styles/global.css';
 import '@fontsource/rubik';
 import '@fontsource/nunito';
-import Layout from './src/components/Layout.js';
+import Layout from './src/components/layout';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import './src/styles/Global.css';
+import './src/styles/global.css';
 import '@fontsource/rubik';
 import '@fontsource/nunito';
-import Layout from './src/components/Layout.js';
+import Layout from './src/components/layout';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
