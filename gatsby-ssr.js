f5f14ab51607adbbf5c2a1d1332b3f8f1d89651d
Matt Domm <mattdomm@gmail.com>
2021-06-04 11:56:07 +0100

    browser update

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './src/styles/Global.css';
 import '@fontsource/rubik';
 import '@fontsource/nunito';
-import Layout from './src/components/Layout';
+import Layout from './src/components/Layout.js';
 
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
